df504b2eb69769b2b0be8e3b03c7e1bec7bea374
Alexandru Madan <alexandru.madan@gmail.com>
2024-05-29 21:05:27 +0200

    more cons

--- src/main/scala/immutable/seq/ListEx.scala
+++ src/main/scala/immutable/seq/ListEx.scala
@@ -1,15 +1,17 @@
 package immutable.seq
 
 object ListEx extends App {
-  // :: - cons operator
+  // :: - Cons operator
   // operators beginning with : apply to the right
   // Nil a singleton, extending List[Nothing], signifies empty list
   //   with the :: operator Nil signifies the end of the list
   val list = 1 :: 2 :: 3 :: Nil
   // desugared form of the above
   val list2 = 1 :: (2 :: (3 :: Nil))
+  // using Cons case class
+  val list3 = ::(1, ::(2, ::(3, Nil)))
 
-  // the cons operator facilitates pattern matching with it
+  // Cons facilitates pattern matching with it
   list match {
     case 1 :: 2 :: _ => println("first 2 elements are 1 and 2")
   }
